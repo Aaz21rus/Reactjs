0200c3b845dd8650e757de89ad26541248a10f98
Aaz21rus <49651870+Aaz21rus@users.noreply.github.com>
2020-07-27 14:49:18 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// import App from './App';
+// import * as serviceWorker from './serviceWorker';
+
+class Form extends React.Component {
+  render() {
+    return (
+      <form className="form-wraper">
+        <div className="container">
+          <h1 className="reg-title">Регистрация</h1>
+          
+          <div className="input-container">
+            <label className="label" htmlFor="email">Введите Email</label>
+            <input className="input" type="text" placeholder="Enter Email" name="email" id="email" required></input>
+          </div>
+          
+          <div className="input-container">
+            <label className="label" htmlFor="psw">Пароль</label>
+            <input className="input" type="password" placeholder="Enter Password" name="psw" id="psw" required></input>
+          </div>
+          
+          <div className="input-container">
+            <label className="label" htmlFor="psw-repeat">Повторите пароль</label>
+            <input className="input" type="password" placeholder="Repeat Password" name="psw" id="psw" required></input>
+          </div>
+          
+          <button type="submit" className="registerbtn">Register</button>
+        </div>
+      </form>
+    )
+  }
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>,
+  <Form />,
   document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
