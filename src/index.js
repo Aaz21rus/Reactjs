33f1fcb1b4702fa098a3f124357319a6339f5c50
Aaz21rus <49651870+Aaz21rus@users.noreply.github.com>
2020-08-03 12:46:25 +0300

    daesn't matter

--- src/index.js
+++ src/index.js
@@ -9,16 +9,16 @@ class Title extends React.Component {
 }
 
 class Input extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {content:''}
-  }
+//   constructor(props) {
+//     super(props)
+//     this.state = {content:''}
+//   }
 
   render() {
     return (
       <div className="input-container">
         <label className="label" htmlFor="email">{this.props.value}</label>
-        <input className="input" type="text" placeholder={this.props.value} name={this.props.name} id={this.props.name} required>{this.state.content}</input>
+        <input className="input" type="text" placeholder={this.props.value} name={this.props.name} id={this.props.name} required></input>
       </div>
     )
   }
