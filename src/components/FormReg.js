a9b7fa2e2f8d7bef368ceff51b4ccf741fc5890b
Dmitriy <Aaz21rus@mail.ru>
2020-08-04 14:18:57 +0300

    onClick

--- src/components/FormReg.js
+++ src/components/FormReg.js
@@ -3,9 +3,16 @@ import Title from './Title'
 import Input from './Input'
 import Btn from './Btn'
 
-const register = () => {alert('Работает!')} 
+// const register = () => {alert('Работает!')} 
 
 class FormReg extends React.Component {  
+  constructor(props) {
+    super(props)
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick() {alert('Работает!')}
+
   render() {
     return (
       <form className="container">
@@ -13,7 +20,8 @@ class FormReg extends React.Component {
         <Input value="Введите email" name="Email" />
         <Input value="Введите пароль" name="psw" />
         <Input value="Подтвердите пароль" name="pswRepeat" />
-        <Btn type="submit" txt="Sign up" action={register} />
+        {/* <Btn type="submit" txt="Sign up" action={register} /> */}
+        <Btn type="submit" txt="Sign up" action={this.handleClick} />
       </form>
     )
   }
